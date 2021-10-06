646ca0d7e12bf7aede6e0264bbe95ebbe421237a
200202iqbal <75594145+200202iqbal@users.noreply.github.com>
2021-10-06 12:29:11 +0900

    消す

--- zaiko_ichiran.php
+++ zaiko_ichiran.php
@@ -12,8 +12,7 @@
 
 //①セッションを開始する
 //session_start();
-var_dump($_SESSION["user"]);
-unset($_SESSION["user"]);
+
 // ②SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
 if ($_SESSION["login"] == false){
 // 	// ③SESSIONの「error2」に「ログインしてください」と設定する。
