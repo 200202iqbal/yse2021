5261dce4bd122005877ac25f844983fc3e65782b
200202iqbal <200202yr@yse-c.net>
2021-11-16 20:13:28 +0900

    complete

--- new_product_kakunin.php
+++ new_product_kakunin.php
@@ -64,6 +64,7 @@ try{
     die($e->getMessage());
 }
 
+<<<<<<< Updated upstream
 function insert($pdo,$data)
 {
     //isbn番号を自動で作る
@@ -88,14 +89,51 @@ function insert($pdo,$data)
 
 if($_SERVER["REQUEST_METHOD"] = "POST"&& isset($_POST["tsuika"]) == "ok")
 {
+=======
+<<<<<<< Updated upstream
+
+=======
+function insert($pdo,$data)
+{
+    //isbn番号を自動で作る
+    // $today = date("ymd");
+    // $data["isbn"] = "9784253".$today;
+    // $data["stock"] += $data["in"];
+
+    $sql = "INSERT INTO books (id,title,author,salesDate,isbn,price,stock,deleteFlag)
+            VALUES (:id,:title,:author,:salesDate,:isbn,:price,:stock,:deleteFlag)";
+    $statement = $pdo->prepare($sql);
+    $item = $statement->execute([
+		":id" => $data["id"],
+        ":title" => $data["title"],
+        ":author" => $data["author"],
+        ":salesDate" => $data["salesDate"],
+        ":isbn" => $data["isbn"],
+        ":price" => $data["itemPrice"],
+        ":stock" => $data["stock"],
+		":deleteFlag" => 0
+    ]);
+    return $item;
+}
+
+if($_SERVER["REQUEST_METHOD"] = "POST" && $_POST["decision"] == 2)
+{
+	$data = $_POST;
+>>>>>>> Stashed changes
 	if(insert($pdo,$data))
 	{
 		$_SESSION["success"] ="新商品追加が完了しました";
 		header("Location: zaiko_ichiran.php");
 		exit;
 	}	
+<<<<<<< Updated upstream
 }
 var_dump($_POST);
+=======
+
+}
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -125,6 +163,7 @@ var_dump($_POST);
 					</thead>
 					<tbody>
 						<tr>
+<<<<<<< Updated upstream
 							<td><?=$data["id"] ?></td>
 							<td><?=$data["title"] ?></td>
 							<td><?=$data["author"] ?></td>
@@ -132,16 +171,54 @@ var_dump($_POST);
 							<td><?=$data["isbn"] ?></td>
 							<td><?=$data["itemPrice"] ?></td>
 							<td><?=$data["stock"] ?></td>	
+=======
+<<<<<<< Updated upstream
+							
+							<td><?php echo	/* ㉟ ㉞で取得した書籍情報からtitleを表示する。 */$selectedBook["title"];?></td>
+							<td><?php echo	/* ㊱ ㉞で取得した書籍情報からstockを表示する。 */$selectedBook["stock"];?></td>
+							<td><?php echo	/* ㊱ POSTの「stock」に設定されている値を㉜の変数を使用して呼び出す。 */$stock = $_POST["stock"][$count]?></td>
+=======
+							<td><?=$data["id"] ?></td>
+							<input type="hidden" name="id" value="<?php echo $data["id"]; ?>">
+							<td><?=$data["title"] ?></td>
+							<input type="hidden" name="title" value="<?php echo $data["title"]; ?>">
+							<td><?=$data["author"] ?></td>
+							<input type="hidden" name="author" value="<?php echo $data["author"]; ?>">
+							<td><?=$data["salesDate"] ?></td>
+							<input type="hidden" name="salesDate" value="<?php echo $data["salesDate"]; ?>">
+							<td><?=$data["isbn"] ?></td>
+							<input type="hidden" name="isbn" value="<?php echo $data["isbn"]; ?>">
+							<td><?=$data["itemPrice"] ?></td>
+							<input type="hidden" name="itemPrice" value="<?php echo $data["itemPrice"]; ?>">
+							<td><?=$data["stock"] ?></td>
+							<input type="hidden" name="stock" value="<?php echo $data["stock"]; ?>">	
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 						</tr>
 					</tbody>
 				</table>
+<<<<<<< Updated upstream
 				<input type="hidden" name="" value="<?php echo $data["id"]; ?>">
+=======
+<<<<<<< Updated upstream
+=======
+				
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 				<div id="kakunin">
 					<p>
 						上記の書籍を登録します。<br>
 						よろしいですか？
 					</p>
+<<<<<<< Updated upstream
 					<button type="submit" id="message" formmethod="POST" name="tsuika" value="ok">はい</button>
+=======
+<<<<<<< Updated upstream
+					<button type="submit" id="message" formmethod="POST" name="add" value="ok">はい</button>
+=======
+					<button type="submit" id="message" formmethod="POST" name="decision" value=2>はい</button>
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 					<button type="submit" id="message" formaction="new_product.php">いいえ</button>
 				</div>
 			</div>
